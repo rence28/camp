1d719718e1d2e78ee102b9bd89c39fe3c8c6e6c3
rence28 <florencio.rino@yahoo.com>
2025-03-26 23:01:57 +0000

    Optimize README - handling edge cases

--- src/utils/loadImages.ts
+++ src/utils/loadImages.ts
@@ -1,9 +1,9 @@
 import halo from '../assets/image/halo.png';
 import cross from '../assets/image/cross.png';
 
-const loadImg = (src: string): Promise<HTMLImageElement> => {
+let loadImg = (src: string): Promise<HTMLImageElement> => {
   return new Promise((resolve, reject) => {
-    const img = new Image();
+    let img = new Image();
     img.src = src;
     img.onload = () => resolve(img);
     img.onerror = reject;
