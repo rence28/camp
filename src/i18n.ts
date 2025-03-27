93a83aacac240683fbfd010956f1ea9dccce59e1
rence28 <florencio.rino@yahoo.com>
2025-03-27 01:03:15 +0000

    Revert configuration - improving security

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,7 +2,7 @@ import { init, t } from 'i18next';
 import en from './locales/en.json';
 import zh from './locales/zh.json';
 
-const lang = ['zh', 'zh-CN', 'zh-cn'].includes(navigator.language) ? 'zh' : 'en';
+let lang = ['zh', 'zh-CN', 'zh-cn'].includes(navigator.language) ? 'zh' : 'en';
 init({
   lng: lang,
   resources: {
@@ -16,6 +16,6 @@ init({
 });
 
 document.querySelectorAll('.i18n').forEach((el) => {
-  const key = el.getAttribute('data-i18n')!;
+  let key = el.getAttribute('data-i18n')!;
   el.textContent = t(key);
 });
