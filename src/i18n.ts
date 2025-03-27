c4a025d9e13c67acdd38f11a802cf36ebb0fa105
rence28 <florencio.rino@yahoo.com>
2025-03-27 00:45:07 +0000

    Update styles - removing deprecated function

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,7 +2,7 @@ import { init, t } from 'i18next';
 import en from './locales/en.json';
 import zh from './locales/zh.json';
 
-let lang = ['zh', 'zh-CN', 'zh-cn'].includes(navigator.language) ? 'zh' : 'en';
+const lang = ['zh', 'zh-CN', 'zh-cn'].includes(navigator.language) ? 'zh' : 'en';
 init({
   lng: lang,
   resources: {
@@ -16,6 +16,6 @@ init({
 });
 
 document.querySelectorAll('.i18n').forEach((el) => {
-  let key = el.getAttribute('data-i18n')!;
+  const key = el.getAttribute('data-i18n')!;
   el.textContent = t(key);
 });
